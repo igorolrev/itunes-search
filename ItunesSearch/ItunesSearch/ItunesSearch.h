6ac4e7a8fdc07a9a91f90e51981b666f0702250a
Tavernari <victortavernari@gmail.com>
2015-06-08 22:39:44 -0300

    initializing project

---
+++ ItunesSearch/ItunesSearch/ItunesSearch.h
@@ -0,0 +1,19 @@
+//
+//  ItunesSearch.h
+//  ItunesSearch
+//
+//  Created by Victor C Tavernari on 08/06/15.
+//  Copyright (c) 2015 Victor C Tavernari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ItunesSearch.
+FOUNDATION_EXPORT double ItunesSearchVersionNumber;
+
+//! Project version string for ItunesSearch.
+FOUNDATION_EXPORT const unsigned char ItunesSearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ItunesSearch/PublicHeader.h>
+
+
